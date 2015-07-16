e469afebe1defedbbdc169836ca46bb950000048
kb0rg <kristenmborges@gmail.com>
2015-07-15 20:33:19 -0700

    generalized functions, so goal str is input at monkey function call and rand_str generation gets len from the goal string.

--- psads_00intro_defFun_monkey.py
+++ psads_00intro_defFun_monkey.py
@@ -24,60 +24,62 @@ print out the best string generated so far and its score every 1000 tries.
 ***kborg note: the goal string is actually 28 char long. revising function.
 """
 
-def generate_string():
+def generate_string(str_len):
 
-	possible_char_string = "abcdefghijklmnopqrstuvwxyz "
-	rand_28_char_string = ""
+	possible_char_str = "abcdefghijklmnopqrstuvwxyz "
+	rand_str = ""
 
-	while len(rand_28_char_string) < 29:
-		rand_28_char_string = rand_28_char_string + random.choice(possible_char_string)
+	while len(rand_str) < (str_len + 1):
+		rand_str = rand_str + random.choice(possible_char_str)
 
-	return rand_28_char_string
+	# print rand_str
+	return rand_str
 
-def score_string(rand_str):
+def score_string(goal_str, test_str):
 
 	"""
-	1 point for every char correct -- in any position
+	find percent of matching characters
 	"""
 
-	score = 28
+	num_char_match = 0
 
-	goal_string = "methinks it is like a weasel"
-
-	if rand_str == goal_string:
-		return score
+	if test_str == goal_str:
+		return 100
 	else:
-		for i in range(0,(len(rand_str) - 1)):
-			if rand_str[i] != goal_string[i]:
-				score -= 1
-		return score
+		for i in range(0,(len(goal_str) - 1)):
+			if test_str[i] == goal_str[i]:
+				num_char_match += 1
+
+	return num_char_match / len(goal_str)
+
+# print score_string("methinks it is like a weasel", generate_string(28))
 
 
-def monkey():
+def monkey(goal_str):
 
 
 	def tries_x1000():
 
 		tries = 0
 		match_goal = False
-		score = 0
+		best_score = 0
 		best_string = ""
 
 
 		while tries <1001:
 		# while tries <1001 and match_goal==False:
-			curr_string = generate_string()
-			curr_score = score_string(curr_string)
+			curr_string = generate_string(len(goal_str))
+			curr_score = score_string(goal_str, curr_string)
 			# print debug
 			# print "current string: " + curr_string
 			# print "current score: " + str(curr_score)
 
-			if curr_score == 28:
+			if curr_score == 100:
 				match_goal = True
 				break
 			else:
-				if curr_score > score:
-					score = curr_score
+				if curr_score >= best_score:
+					best_score = curr_score
 					best_string = curr_string	
 				tries += 1
 				# print debug
@@ -85,7 +87,7 @@ def monkey():
 			
 		if match_goal == False:
 			print "the best string in 1000 tries is: " + best_string + \
-				"\n and its score is: " + str(curr_score)
+				"\n and its score is: " + str(curr_score) + "%"
 			tries = 0
 			tries_x1000()
 
@@ -97,6 +99,7 @@ def monkey():
 
 	tries_x1000()
 
-monkey()
+monkey("methinks it is like a weasel")
+
 
 
