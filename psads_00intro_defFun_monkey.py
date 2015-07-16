5315900674e0d5acdcd1c2f8b266c1266bd14c37
kb0rg <kristenmborges@gmail.com>
2015-07-15 20:03:58 -0700

    modified string length to match target string.

--- psads_00intro_defFun_monkey.py
+++ psads_00intro_defFun_monkey.py
@@ -8,7 +8,7 @@ How long do you think it would take for a Python function to generate just one
 sentence of Shakespeare? The sentence we’ll shoot for is:
 "methinks it is like a weasel"
 
-write a function that generates a string that is 27 characters long by 
+write a function that generates a string that is 27**** characters long by 
 choosing random letters from the 26 letters in the alphabet plus the space.
 
 We’ll write another function that will score each generated string by comparing 
@@ -20,17 +20,19 @@ generate a whole new string.
 
 To make it easier to follow your program’s progress this third function should 
 print out the best string generated so far and its score every 1000 tries.
+
+***kborg note: the goal string is actually 28 char long. revising function.
 """
 
 def generate_string():
 
 	possible_char_string = "abcdefghijklmnopqrstuvwxyz "
-	rand_27char_string = ""
+	rand_28_char_string = ""
 
-	while len(rand_27char_string) < 28:
-		rand_27char_string = rand_27char_string + random.choice(possible_char_string)
+	while len(rand_28_char_string) < 29:
+		rand_28_char_string = rand_28_char_string + random.choice(possible_char_string)
 
-	return rand_27char_string
+	return rand_28_char_string
 
 def score_string(rand_str):
 
@@ -38,7 +40,7 @@ def score_string(rand_str):
 	1 point for every char correct -- in any position
 	"""
 
-	score = 27
+	score = 28
 
 	goal_string = "methinks it is like a weasel"
 
@@ -70,9 +72,9 @@ def monkey():
 			# print "current string: " + curr_string
 			# print "current score: " + str(curr_score)
 
-			if curr_score == 27:
+			if curr_score == 28:
+				match_goal = True
 				break
-				# match_goal = True
 			else:
 				if curr_score > score:
 					score = curr_score
@@ -81,15 +83,20 @@ def monkey():
 				# print debug
 				# print "current try: " + str(tries)
 			
-		print "the best string in 1000 tries is: " + best_string + \
-			"\n and its score is: " + str(curr_score)
-		# tries = 0
-		tries_x1000()
+		if match_goal == False:
+			print "the best string in 1000 tries is: " + best_string + \
+				"\n and its score is: " + str(curr_score)
+			tries = 0
+			tries_x1000()
 
-		# print debug
-		print "final string: " + curr_string
-		print "It's a match!"
+		else:
+			# print debug
+			print "final string: " + curr_string
+			print "It's a match!"
+			return
 
 	tries_x1000()
+
+monkey()
 
 
