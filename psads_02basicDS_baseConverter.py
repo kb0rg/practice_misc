6e740b62ca57da82f2b4cf6aa7f348ec2337d2b4
kb0rg <kristenmborges@gmail.com>
2015-07-16 15:43:32 -0700

    Base converters (up to hex) from PSWADS. requires pythonds module.

---
+++ psads_02basicDS_baseConverter.py
@@ -0,0 +1,35 @@
+from pythonds.basic.stack import Stack
+
+def baseConverter(decNumber,base):
+
+    """
+    converts up to hexidecimal
+    """
+
+    digits = "0123456789ABCDEF"
+
+    remstack = Stack()
+
+    if base > 16 and decNumber != base:
+        return "Sorry! Except in special cases, this function only works up to hexidecimal (base 16)"
+
+    if decNumber == base:
+        return 10
+
+    while decNumber > 0:
+        rem = decNumber % base
+        remstack.push(rem)
+        decNumber = decNumber // base
+
+    newString = ""
+    while not remstack.isEmpty():
+        newString = newString + digits[remstack.pop()]
+
+    return newString
+
+print "25 base 2 is: " + str((baseConverter(25,2)))
+print "25 base 8 is: " + str((baseConverter(25,8)))
+print "25 base 16 is: " + str((baseConverter(25,16)))
+print "256 base 16 is: " + str((baseConverter(256,16)))
+print "25 base 26 is: " + str((baseConverter(25, 26)))
+print "26 base 26 is: " + str((baseConverter(26, 26)))
