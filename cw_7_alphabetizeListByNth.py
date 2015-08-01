18df03779266befcd0ef2401f78e72659306ae77
kb0rg <kristenmborges@gmail.com>
2015-07-31 23:51:16 -0700

    added notes to help direct next pass.

--- cw_7_alphabetizeListByNth.py
+++ cw_7_alphabetizeListByNth.py
@@ -41,6 +41,27 @@ if __name__ == "__main__":
 	import doctest
 	doctest.testmod()
 
+"""
+this worked on the first round of tests. then failed in the second:
+some of the input strings were separated by "," only instead of ", "
+
+also, some tests have duplicates, which will break current structure using dict.
+re-work how values are stored (use list)
+also, keys should poss be the nth char only, not the string slice, to fix issue with
+different strings that have the same nth char:
+currently returns "ties" sorted alphabetically, but looks like, after initial sort, it should return in order passed
+
+eg:
+>>> sort_it('Wolfgang Mittermeyer, Neidhardt Muller, Ernst von Eisenach, Theodor von Lucke, Hildegard von Mariendorf, Adalbert von Fahrenheit, Ernst von Eisenach, Karl Gustav Kempf', 4)
+'Hildegard von Mariendorf, Neidhardt Muller, Wolfgang Mittermeyer, Karl Gustav Kempf, Adalbert von Fahrenheit, Theodor von Lucke, Ernst von EisenachErnst von Eisenach' 
+should equal 
+'Neidhardt Muller, Hildegard von Mariendorf, Wolfgang Mittermeyer, Adalbert von Fahrenheit, Karl Gustav Kempf, Theodor von Lucke, Ernst von Eisenach, Ernst von Eisenach'
+
+>>> sort_it('Adalbert von Fahrenheit, Theodor von Lucke, Ernst von Eisenach, Ernst von Eisenach, August Samuel Wahlen, Paul von Oberstein', 9)
+'Adalbert von Fahrenheit, Paul von Oberstein, August Samuel Wahlen, Ernst von Eisenach, Ernst von Eisenach, Theodor von Lucke'
+
+"""
+
 
 # print sort_it('bid, zag', 2)
 # print "*" * 30
