d006cb1a44f03c3715fce9ef5bb3720f84cf38cb
kb0rg <kristenmborges@gmail.com>
2015-08-10 12:27:31 -0700

    altered test cases to pass doctests.

--- cw_8_unfinishedLoopBugFix01.py
+++ cw_8_unfinishedLoopBugFix01.py
@@ -5,13 +5,13 @@ find and fix the bug in this unfinished For Loop!
 >>> create_array(1)
 [1]
 >>> create_array(2)
-[1,2]
+[1, 2]
 >>> create_array(3)
-[1,2,3]
+[1, 2, 3]
 >>> create_array(4)
-[1,2,3,4]
+[1, 2, 3, 4]
 >>> create_array(5)
-[1,2,3,4,5]
+[1, 2, 3, 4, 5]
 """
 
 def create_array(n):
