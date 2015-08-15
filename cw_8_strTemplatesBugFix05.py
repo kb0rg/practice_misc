8dc0ce2171ae83e3811bb9b47092a3b92bc5c7ce
kb0rg <kristenmborges@gmail.com>
2015-08-14 23:45:14 -0700

    was not passing doctests. looked up solution, had it right. passes on cw but still not in doctests. confused.

--- cw_8_strTemplatesBugFix05.py
+++ cw_8_strTemplatesBugFix05.py
@@ -10,9 +10,23 @@ fix string template
 """
 
 def build_string(*args):
-    return "I like {}!".format(",".join(args))
+    return "I like {}!".format(", ".join(args))
 
 if __name__ == "__main__":
 
 	import doctest
 	doctest.testmod()
+
+"""
+??? 
+passes cw but not doctests.
+
+doctest results showing ", " AFTER last arg.
+also returning string inside parens.
+
+some of the other solutions on cw puzzle me.
+why would this work?:	
+    return "I like {0}!".format(", ".join(args))
+(why does it returns more than 1st arg?)
+
+"""
