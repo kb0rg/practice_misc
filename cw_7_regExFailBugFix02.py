df8bcfc1a134be61d2a1cbf1c0f40311abc7c8d5
kb0rg <kristenmborges@gmail.com>
2015-08-19 15:14:35 -0700

    code before bugfix.

---
+++ cw_7_regExFailBugFix02.py
@@ -0,0 +1,18 @@
+"""
+fix regex filter
+
+>>> filter_words("You're Bad! timmy!")
+'You're awesome! timmy!'
+>>> filter_words("You're MEAN! timmy!")
+'You're awesome! timmy!'
+"""
+
+from re import 
+
+def filter_words(phrase):
+    return sub("(bad|mean|ugly|horrible|hideous)","awesome",phrase)
+
+if __name__ == "__main__":
+
+	import doctest
+	doctest.testmod()
