3ace6a699fc61d878c1f25700e839960bfb65885
kb0rg <kristenmborges@gmail.com>
2016-02-23 21:59:08 -0800

    problem 1 from pythonchallenge. description before tackling.

---
+++ pc_1.py
@@ -0,0 +1,18 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+"""
+Python Challenge 1
+http://www.pythonchallenge.com/pc/def/map.html
+
+image:
+K -> M 
+O -> Q 
+E -> G 
+
+hint:
+everybody thinks twice before solving this.
+
+text:
+g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.
+
+"""
