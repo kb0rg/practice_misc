dd36a6a9409e3f0e35c65364ed1675eb06996986
kb0rg <kristenmborges@gmail.com>
2016-02-24 22:24:07 -0800

    python challange puzzle 2.

---
+++ pc_2.py
@@ -0,0 +1,27 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+"""
+Python Challenge 2
+http://www.pythonchallenge.com/pc/def/map.html
+
+image:
+an open book (text not legible)
+
+hint:
+recognize the characters. maybe they are in the book, 
+but MAYBE they are in the page source.
+
+
+"""
+
+# import string
+
+# def decode_str(text):
+
+#     alphabet = list(string.ascii_lowercase)
+#     cracked = alphabet[2:] + alphabet[:2]
+
+
+
+# output: 
+# 
