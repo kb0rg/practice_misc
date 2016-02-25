47e9721208e68950633d471cb6b929ed112a9d70
kb0rg <kristenmborges@gmail.com>
2016-02-25 15:49:35 -0800

    python challenge problem 4. problem statement only.

---
+++ pc_4.py
@@ -0,0 +1,30 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+"""
+Python Challenge 4
+http://www.pythonchallenge.com/pc/def/linkedlist.php
+
+image:
+two wooden dolls on either side of a log, with two-handle saw
+
+hint:
+image hover text:
+linkedlist.php?nothing=12345
+
+hint2:
+title = follow the chain
+
+hint3:
+comment in source:
+urllib may help. DON'T TRY ALL NOTHINGS, since it will never 
+end. 400 times is more than enough.
+
+note:
+and the next nothing is 44827...
+and the next nothing is 45439...
+
+approach:
+
+questions:
+
+"""
