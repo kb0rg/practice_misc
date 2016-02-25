0b308f397651ba5cd95c4c6ecb1fa2a01e44de52
kb0rg <kristenmborges@gmail.com>
2016-02-25 13:30:34 -0800

    got solution by visually sorting the anagram, will revisit to find a programtic solution.

--- pc_3.py
+++ pc_3.py
@@ -15,4 +15,30 @@ note:
 in page source is another giant block of char. 
 saved as py_3.txt
 
+approach:
+we want to find a lowercase char with 3 uppercase char on either side. 
+those need to be bounded by lowercase to fullfill the "exactly 3" parameter.
+
+look for pattern sLLLsLLLs
+
+questions:
+how to do this more efficiently than looking for pattern at each char?
+how are returns treated? is each line distinct or does pattern wrap?
+
 """
+
+f = open("pc_3.txt")
+
+for line in f:
+    i = 0
+    while i < (len(line) - 8): # check to last poss occur of 1st char in pattern
+        if not line[i].islower():
+            i += 1
+        else: 
+            if line[i:i+4].isupper():
+                
+            i+=1
+print all_lowers
+
+
+
