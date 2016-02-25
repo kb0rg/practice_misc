b77b502dcc9fadcdb4117146264b7bcc48787b93
kb0rg <kristenmborges@gmail.com>
2016-02-25 12:12:00 -0800

    python challenge 3, problem description.

---
+++ pc_3.py
@@ -0,0 +1,18 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+"""
+Python Challenge 3
+http://www.pythonchallenge.com/pc/def/equality.html
+
+image:
+7 candles: one small with 3 large on either side
+
+hint:
+One small letter, surrounded by EXACTLY three big bodyguards 
+on each of its sides.
+
+note:
+in page source is another giant block of char. 
+saved as py_3.txt
+
+"""
