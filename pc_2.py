9bdc454ff23ad9df07d799f9741f3dad3964bc77
kb0rg <kristenmborges@gmail.com>
2016-02-24 22:45:55 -0800

    partial solution. text file contains text to search, have identified counts of each char. need to order output.

--- pc_2.py
+++ pc_2.py
@@ -11,17 +11,58 @@ hint:
 recognize the characters. maybe they are in the book, 
 but MAYBE they are in the page source.
 
+note:
+in page source is following comment:
+find rare characters in the mess below:
+
+followed by a giant block of symbols (1220 lines)
+saved as py_2.txt
 
 """
+from pprint import pprint
 
-# import string
+f = open("pc_2.txt")
 
-# def decode_str(text):
+char_counts = {}
 
-#     alphabet = list(string.ascii_lowercase)
-#     cracked = alphabet[2:] + alphabet[:2]
+for line in f:
+    for char in line:
+        char_counts[char] = char_counts.get(char, 0) + 1
 
+pprint(char_counts)
 
+"""
+output: 
 
-# output: 
-# 
+{'\n': 1219,
+ '!': 6079,
+ '#': 6115,
+ '$': 6046,
+ '%': 6104,
+ '&': 6043,
+ '(': 6154,
+ ')': 6186,
+ '*': 6034,
+ '+': 6066,
+ '@': 6157,
+ '[': 6108,
+ ']': 6152,
+ '^': 6030,
+ '_': 6112,
+ 'a': 1,
+ 'e': 1,
+ 'i': 1,
+ 'l': 1,
+ 'q': 1,
+ 't': 1,
+ 'u': 1,
+ 'y': 1,
+ '{': 6046,
+ '}': 6105}
+
+ok. round the rare charaters. but in what order should they be typed?
+"aeilqtuy" -- order of output above -- didn't work.
+since dictionaries are unordered, output is not sequential. 
+another data type?
+
+"""
