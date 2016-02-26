bc85c8577ee35c026906f2ac9dbca82723b633ab
kb0rg <kristenmborges@gmail.com>
2016-02-25 17:28:57 -0800

    solution wip. following nexts and looking for a break in the pattern.

--- pc_4.py
+++ pc_4.py
@@ -25,6 +25,53 @@ and the next nothing is 45439...
 
 approach:
 
+loop through a range (up to 400)
+use urllib to open page, read html text.
+grab numbers out of html body
+rebind nothing to numbers
+return last url
+
 questions:
 
+are numbers always same number of digits? 
+no
+is body text before numbers always the same?
+no
+then need to find a way to grab numbers out of text w/o string slicing
+
 """
+from pprint import pprint
+import urllib2
+
+def chainsaw():
+
+    base_url = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="
+    num = "12345"
+    num_dict={}
+
+    for x in range(401):
+        new_url = base_url + num
+        response = urllib2.urlopen(new_url)
+        html = response.read()
+        # grab numbers out of html body
+        num = ''.join([str(i) for i in html.split() if i.isdigit()])
+        num_dict[num] = num_dict.get(num, 0) + 1
+    
+    return num_dict
+
+pprint(chainsaw())
+
+
+"""
+following the nothings 400 times returns:
+http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=85501
+which gives:
+and the next nothing is 43650
+
+creating a dict to see what is going on. is the list circular?
+
+there are several numbers that appear only once, and some twice.
+there are also a couple of empty results.
+
+"""
+
