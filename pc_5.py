e0402d720a4fc4d4d22c1100cc621674e4e2bfde
kb0rg <kristenmborges@gmail.com>
2016-02-26 12:21:41 -0800

    python challenge problem 5.

---
+++ pc_5.py
@@ -0,0 +1,32 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+"""
+Python Challenge 5
+http://www.pythonchallenge.com/pc/def/peak.html
+
+image:
+a hill
+
+hint:
+page title: peak hell
+
+hint2:
+peak hell sounds familiar ?
+
+note:
+sounds like... pickle?
+I know of it, don't really know what it is
+
+approach:
+look into pickle module.
+look for something to unpickle
+try:
+<peakhell src="banner.p"/>
+
+questions:
+
+"""
+
+
+"""
+"""
