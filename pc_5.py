8235b8dea20e8ade4f280ac1e8ea202c77ca10fd
kb0rg <kristenmborges@gmail.com>
2016-02-26 23:14:10 -0800

    wip. trying out some ideas.

--- pc_5.py
+++ pc_5.py
@@ -40,13 +40,19 @@ def peak_hill():
     
     # peakhell is a list of lists of tuples. 
     # looks like most sum 95. see if there are any that don't?
+    count = 0
+    sub_count_str = ''
     for peak in peakhell:
         sub_count = 0
         for duo in peak:
             sub_count += duo[1]
-        print sub_count
+        count += sub_count
+        sub_count_str = sub_count_str + str(sub_count)
+        
+    print sub_count_str
+    print count
 
-print peak_hill()
+peak_hill()
 
 """
 result:
@@ -58,7 +64,12 @@ first and last list are: [(' ', 95)]
 
 the sum of all intergers in each list is 95.
 are the hash signs significant?
-should I add all the 95s?
-should I cat all the 95s?
 
+should I add all the 95s?
+http://www.pythonchallenge.com/pc/def/2185.html 
+nope.
+
+should I cat all the 95s?
+http://www.pythonchallenge.com/pc/def/9595959595959595959595959595959595959595959595.html
+nope.
 """
