ab6b3e2b26674d122ac206d9cf44121c769e0d02
kb0rg <kristenmborges@gmail.com>
2016-02-26 22:58:59 -0800

    pickle solution wip.

--- pc_5.py
+++ pc_5.py
@@ -14,8 +14,7 @@ hint2:
 peak hell sounds familiar ?
 
 note:
-sounds like... pickle?
-I know of it, don't really know what it is
+sounds like... pickle
 
 approach:
 look into pickle module.
@@ -24,9 +23,30 @@ try:
 <peakhell src="banner.p"/>
 
 questions:
+what to do with the results of the unpickle?
+or is there something else I need to unpickle? 
 
 """
+import pickle
+from pprint import pprint
 
+fileObject = open('pc_5_banner.p', 'r')
+# fileObject = open('pc_5_peakhell.jpg', 'r')
+peakhell = pickle.load(fileObject)
+
+pprint(peakhell)
 
 """
+result:
+a long list of lists. of tuples. each tuple has an int in index 1. 
+every other tuple containing a hash sign in index 0, and
+the alternates have an empty string in index 0.
+
+first and last list are: [(' ', 95)]
+
+other than that not seeing a clear pattern. the sublists contain varying 
+numbers of tuples. I'm sure there's something I'm not seeing yet.
+
+tried unpickling the hill image but got KeyError
+
 """
