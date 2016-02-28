0f81116684ad92a4502ac77205952a490a50811a
kb0rg <kristenmborges@gmail.com>
2016-02-28 00:48:42 -0800

    solution.

--- pc/pc_5.py
+++ pc/pc_5.py
@@ -25,22 +25,9 @@ what to do with the results of the unpickle?
 
 """
 import pickle
-from pprint import pprint
 import urllib2
-# import matplotlib.pyplot as plt
-# import numpy as np
 
 def peak_hill():
-
-    fileObject = open('pc_5_banner.p', 'rb')
-    peakhell = pickle.load(fileObject)
-
-    # do I need to re-pickle it for some reason? 
-    # re_peakhell = open('temp_pc_5.pkl', 'wb')
-    ## maybe as ascii?
-    # re_peakhell = open('temp_pc_5.pkl', 'wa')
-    # pickle.dump(peakhell, re_peakhell)
-    # re_peakhell.close()
 
     url = 'http://www.pythonchallenge.com/pc/def/banner.p'
     data = pickle.loads(urllib2.urlopen(url).read())
@@ -51,18 +38,13 @@ peak_hill()
 
 """
 result:
-a long list of lists. of tuples. each tuple has an int in index 1. 
-every other tuple containing a hash sign in index 0, and
-the alternates have an empty string in index 0.
 
+got stuck. was trying to visually plot the data, looked into matplotlib
+but was getting errors, couldn't figure it out. 
 
-## 
-
-tried to modify a coulple matplotlib examples fr stack overflow
-but kept getting errors I couldn't sort out (UserWarning: Dulpicate key in file 
-"..../.matplotlib/matplotlibrc", line #2)
-
-need to learn matplotlib.
-
+reached my stuckness threshhold. looked up solution. arrrgh.
+same idea on a high level (visual representation of data). 
+but totally different approach.
+prints "channel"
 """
 
