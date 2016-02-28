23b71b9fbebe9415dbff611da441347a91260299
kb0rg <kristenmborges@gmail.com>
2016-02-28 00:54:59 -0800

    python challenge 6, problem description.

---
+++ pc/pc_6.py
@@ -0,0 +1,31 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+"""
+Python Challenge 6
+http://www.pythonchallenge.com/pc/def/channel.html
+
+image:
+shiny black pants with zipper open.
+colorful shiny underthing with super 70s pattern.
+an arrow pointing toward belt loop.
+
+page title:
+"now there are pairs"
+
+hint:
+comment "<-- zip -->" at opening html tag
+
+hint2:
+
+note:
+paypal button: html comment says not relevant to riddle.  
+
+approach:
+
+questions:
+
+"""
+
+
+"""
+"""
