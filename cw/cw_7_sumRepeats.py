4dad1e5b44e24ac677594b1c4ece9216fc112ee1
kb0rg <kristenmborges@gmail.com>
2016-08-06 21:53:23 -0700

    Missing non-adjacent intersections.

--- cw/cw_7_sumRepeats.py
+++ cw/cw_7_sumRepeats.py
@@ -7,15 +7,16 @@ list. Now that might have sounded confusing, it isn't:
 import unittest
 
 def repeat_sum(list_of_lists):
+	
+	if len(list_of_lists)<2:
+		return 0
 
 	list_sets = [set(l) for l in list_of_lists]
-	print 'list_sets', list_sets
-	repeats = []
-	for i in range(len(list_sets)-2):
-		repeats.append(list_sets[i] & list_sets[i+1])
-	print 'repeats', repeats
-	# print 'set(repeats)', set(repeats)
-	# return sum(int(i) for i in set(repeats))
+	repeats = set()
+	for i in range(len(list_sets)-1):
+		list_sets[i].intersection(list_sets[i+1])
+
+	return sum(repeats)
 
 class TestPrintRepeatSum(unittest.TestCase):
     def test_base_case(self):
@@ -30,6 +31,10 @@ class TestPrintRepeatSum(unittest.TestCase):
         input_list = [[1, 8, 8], [8, 8, 8], [8, 8, 8, 1]]
         # sum of [1,8]
         self.assertEqual(repeat_sum(input_list), 9)
+    def test_single_list(self):
+        input_list = [[1]]
+        # sum of []
+        self.assertEqual(repeat_sum(input_list), 0)
 
 if __name__ == "__main__":
 
