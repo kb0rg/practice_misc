e5d086680c19a43a5d964307e68f843f5c78e441
kb0rg <kristenmborges@gmail.com>
2016-09-24 16:44:06 -0700

    Playing with pairs to zip.

--- pc/pc_6.py
+++ pc/pc_6.py
@@ -13,19 +13,39 @@ page title:
 "now there are pairs"
 
 hint:
-comment "<-- zip -->" at opening html tag
-
-hint2:
+comment "<-- zip" at opening html tag
 
 note:
 paypal button: html comment says not relevant to riddle.  
+pants button says "sprint" twice
 
 approach:
+looking into zipfile
 
 questions:
+what to zip? the jpg? then what?
+"""
+import pickle
+import urllib2
+
+def zipper():
+    img_url = 'http://www.pythonchallenge.com/pc/def/channel.jpg'
+    txt_url = 'http://www.pythonchallenge.com/pc/def/channel.html'
+
+    img_data = urllib2.urlopen(img_url).read()
+    txt_data = urllib2.urlopen(txt_url).read()
+
+    import pdb; pdb.set_trace()
+    zz = zip(img_data, txt_data)
+
+
+if __name__ == '__main__':
+    zipper()
 
 """
+reading the image as data, can then zip(), which returns a list of tuples.
+passing only one iternable returns single-item tuples. 
+hint about pairs...? pass in same image twice? pass in html and img?
 
 
-"""
 """
