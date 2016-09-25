4563c4594f842ffe308ca8a46e4bcc177fc57f20
kb0rg <kristenmborges@gmail.com>
2016-09-25 00:31:14 -0700

    Notes/ more hints.

--- pc/pc_6.py
+++ pc/pc_6.py
@@ -23,29 +23,29 @@ approach:
 looking into zipfile
 
 questions:
-what to zip? the jpg? then what?
+
+arrrrrrgh. gave up, and looked for hint. got the answer in the search result,
+not just a hint. sigh. well, still need to implement it.
+
+hint "<-- zip" at opening tag was pointing to the tag (html).
+replace "html with "zip" and open 
+http://www.pythonchallenge.com/pc/def/channel.zip
+
+unzip, find many numbered txt files and a readme.
+readme says:
+"welcome to my zipped list.
+hint1: start from 90052
+hint2: answer is inside the zip"
+
+90052.txt says:
+"Next nothing is 94191"
+(redux of pc_4 linked list)
+
 """
-import pickle
-import urllib2
-
 def zipper():
-    img_url = 'http://www.pythonchallenge.com/pc/def/channel.jpg'
-    txt_url = 'http://www.pythonchallenge.com/pc/def/channel.html'
+    pass
 
-    img_data = urllib2.urlopen(img_url).read()
-    txt_data = urllib2.urlopen(txt_url).read()
-
-    import pdb; pdb.set_trace()
-    zz = zip(img_data, txt_data)
 
 
 if __name__ == '__main__':
     zipper()
-
-"""
-reading the image as data, can then zip(), which returns a list of tuples.
-passing only one iternable returns single-item tuples. 
-hint about pairs...? pass in same image twice? pass in html and img?
-
-
-"""
