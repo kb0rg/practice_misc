ee4b8e3f5e81447104deadd11436b8d3dbf55bc0
kb0rg <kristenmborges@gmail.com>
2016-10-09 21:23:44 -0700

    WIP

--- pc/pc_6.py
+++ pc/pc_6.py
@@ -42,10 +42,33 @@ hint2: answer is inside the zip"
 (redux of pc_4 linked list)
 
 """
+import os
+
 def zipper():
-    pass
+    dirpath = '~/Downloads/channel/'
+    nums = '90052'
 
-
+    while nums:
+        filename = ''.join([dirpath, nums, '.txt'])
+        nextfile = open(os.path.expanduser(filename), 'r')
+        msg = nextfile.read()
+        clue = nextfile.comment()
+        print msg
+        print "clue: ", clue
+        nums = msg[16:] #strips 'Next nothing is '
+        nextfile.close()
 
 if __name__ == '__main__':
     zipper()
+
+"""
+After reading and redirecting to several "next" nums, got:
+"Next nothing is 46145
+Collect the comments."
+
+comments?
+"""
+
+with ZipFile('spam.zip') as myzip:
+    with myzip.open('eggs.txt') as myfile:
+        print(myfile.read())
