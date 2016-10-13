04b89d280c8d31859e56e75a248a24b58cddb356
kb0rg <kristenmborges@gmail.com>
2016-10-12 19:47:02 -0700

    Got hint, trying out PIL.

--- pc/pc_7.py
+++ pc/pc_7.py
@@ -15,28 +15,23 @@ page title:
 hint:
 
 note:
+got stuck, looked for hints. apparently pillow is the way to go,
+and smarty is not relevant as a hint to the approach.
 
 approach:
-smarty or smartypants modules?
-neither seems relevant
-decode image to string?
+examine pixel values using PIL.
+turn values into letters.
 
 questions:
 
-
 """
+from PIL import Image
 import urllib2
-import base64
 
 def oxygen():
     url = 'http://www.pythonchallenge.com/pc/def/oxygen.png'
     import pdb; pdb.set_trace()
-    # pic = urllib2.urlopen(url)
-    # with open(pic, "rb") as imageFile:
-    #     str = base64.b64encode(imageFile.read())
-    #     print str
-    pic = urllib2.urlopen(url).read()
-    for line in pic:
-        print line
+    pic = urllib2.urlopen(url)
+    import pdb; pdb.set_trace()
 if __name__ == '__main__':
     oxygen()
