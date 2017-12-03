ab43313eed640fd638772f9c39bc95daea02319b
kb0rg <kb0rg@users.noreply.github.com>
2017-12-02 19:46:37 -0800

    Fix for apostrophes

--- ex/python/word-count/word_count.py
+++ ex/python/word-count/word_count.py
@@ -1,5 +1,5 @@
 import re
 
 def word_count(phrase):
-    words = re.split('[\W_]+', phrase.lower())
+    words = re.split("[\W_]+(?<!')", phrase.lower())
     return {w: words.count(w) for w in words if w}
