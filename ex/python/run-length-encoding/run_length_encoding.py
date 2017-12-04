88019e8a490e83f027785c2acae5a56492a9e80d
kb0rg <kb0rg@users.noreply.github.com>
2017-12-03 20:14:25 -0800

    Add encode func. Passes all tests

--- ex/python/run-length-encoding/run_length_encoding.py
+++ ex/python/run-length-encoding/run_length_encoding.py
@@ -18,4 +18,22 @@ def decode(in_str):
     return ''.join(out_str)
 
 def encode(in_str):
-    pass
+    if not in_str:
+        return in_str
+
+    out_str = [in_str[0]]
+    count = 1
+
+    for i in range(1,len(in_str)):
+        if in_str[i] == out_str[-1]:
+            count += 1
+            if i == len(in_str)-1:
+                last = out_str.pop()
+                out_str.extend([str(count), last])
+        else:
+            if count > 1:
+                last = out_str.pop()
+                out_str.extend([str(count), last])
+            out_str.append(in_str[i])
+            count = 1
+    return ''.join(out_str)
