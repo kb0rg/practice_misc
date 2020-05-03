cfd549cc284ad47ad032b063fa7c8f9152b483f9
kb0rg <kb0rg@users.noreply.github.com>
2020-05-03 11:50:37 -0700

    Sketch out initial thoughts and a little code

--- leet/1021_remove_outer_parentheses.py
+++ leet/1021_remove_outer_parentheses.py
@@ -56,3 +56,30 @@ S is a valid parentheses string
 
 class Solution:
     def removeOuterParentheses(self, S: str) -> str:
+        return_substrings = []
+        primitive_substrings = []
+
+        substring_stack = []
+        start_index = 0
+        end_index = 0
+
+        # check for valid primitives by looking for complete sets of "()" chars
+        for char in S:
+            if not len(substring_stack):
+                substring_stack.append(char)
+                end_index += 1
+            else:
+                if char == "(":
+                    substring_stack.append(char)
+                    end_index += 1
+                else:
+                    # if last char in stack is ")", append char
+                    # else pop last char in stack
+
+            # once I've identified a valid primitive, add it to a list
+            primitive_substrings.append(S[start_index, end_index + 1])
+
+        # then iterate through that list and remove the outer parens
+
+        # return remaining substrings as a string
+        return return_substrings.join()
