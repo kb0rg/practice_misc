12a5f8c49e49ad305fd85e42f807d91cd4e96da7
kb0rg <kb0rg@users.noreply.github.com>
2020-05-09 21:51:00 -0700

    Convert examples to unit tests

--- leet/859_buddy_strings.py
+++ leet/859_buddy_strings.py
@@ -2,29 +2,8 @@
 859. Buddy Strings
 https://leetcode.com/problems/buddy-strings/
 
-Given two strings A and B of lowercase letters, return true if and only if we can swap two letters in A so that the result equals B.
-
-Example 1:
-
-Input: A = "ab", B = "ba"
-Output: true
-Example 2:
-
-Input: A = "ab", B = "ab"
-Output: false
-Example 3:
-
-Input: A = "aa", B = "aa"
-Output: true
-Example 4:
-
-Input: A = "aaaaaaabc", B = "aaaaaaacb"
-Output: true
-Example 5:
-
-Input: A = "", B = "aa"
-Output: false
-
+Given two strings A and B of lowercase letters, return true if and only if we
+can swap two letters in A so that the result equals B.
 
 Note:
 
@@ -34,6 +13,31 @@ A and B consist only of lowercase letters.
 
 """
 
+import unittest
+
 class Solution:
     def buddyStrings(self, A: str, B: str) -> bool:
         pass
+
+class Tests(unittest.TestCase):
+
+    sol = Solution()
+
+    def test_example_1(self):
+        self.assertEqual(self.sol.buddyStrings("ab", "ba"), True)
+
+    def test_example_2(self):
+        self.assertEqual(self.sol.buddyStrings("ab", "ab"), False)
+
+    def test_example_3(self):
+        self.assertEqual(self.sol.buddyStrings("aa", "aa"), True)
+
+    def test_example_4(self):
+        self.assertEqual(self.sol.buddyStrings("aaaaaaabc", "aaaaaaacb"), True)
+
+    def test_example_5(self):
+        self.assertEqual(self.sol.buddyStrings("", "aa"), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
