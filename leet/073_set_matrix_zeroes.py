6245e8be9581dea357cafe569420545eab5a6a44
kb0rg <kb0rg@users.noreply.github.com>
2020-05-11 12:13:52 -0700

    Convert examples to unit tests

--- leet/073_set_matrix_zeroes.py
+++ leet/073_set_matrix_zeroes.py
@@ -5,37 +5,6 @@ https://leetcode.com/problems/set-matrix-zeroes/
 Given a m x n matrix, if an element is 0, set its entire row and column to 0.
 Do it in-place.
 
-Example 1:
-
-Input:
-[
-  [1,1,1],
-  [1,0,1],
-  [1,1,1]
-]
-Output:
-[
-  [1,0,1],
-  [0,0,0],
-  [1,0,1]
-]
-
-Example 2:
-
-Input:
-[
-  [0,1,2,0],
-  [3,4,5,2],
-  [1,3,1,5]
-]
-Output:
-[
-  [0,0,0,0],
-  [0,4,5,0],
-  [0,3,1,0]
-]
-
-
 Follow up:
 
 A straight forward solution using O(mn) space is probably a bad idea.
@@ -43,9 +12,51 @@ A simple improvement uses O(m + n) space, but still not the best solution.
 Could you devise a constant space solution?
 """
 
+from typing import List
+import unittest
+
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
         pass
+
+class Test(unittest.TestCase):
+
+    sol = Solution()
+
+    def test_example_1(self):
+        input = [
+          [1,1,1],
+          [1,0,1],
+          [1,1,1]
+        ]
+
+        output = [
+          [1,0,1],
+          [0,0,0],
+          [1,0,1]
+        ]
+        self.sol.setZeroes(input)
+        self.assertEqual(input, output)
+
+    def test_example_2(self):
+
+        input = [
+          [0,1,2,0],
+          [3,4,5,2],
+          [1,3,1,5]
+        ]
+
+        output = [
+          [0,0,0,0],
+          [0,4,5,0],
+          [0,3,1,0]
+        ]
+        self.sol.setZeroes(input)
+        self.assertEqual(input, output)
+
+
+if __name__ == '__main__':
+    unittest.main()
