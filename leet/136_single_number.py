514e7d96076933206d5de803da8e8712bab89553
kb0rg <kb0rg@users.noreply.github.com>
2020-05-15 16:37:27 -0700

    Convert examples to unit tests

--- leet/136_single_number.py
+++ leet/136_single_number.py
@@ -9,18 +9,25 @@ Note:
 Your algorithm should have a linear runtime complexity. Could you implement it
 without using extra memory?
 
-Example 1:
-
-Input: [2,2,1]
-Output: 1
-
-Example 2:
-
-Input: [4,1,2,1,2]
-Output: 4
-
 """
+from typing import List
+import unittest
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         pass
+
+
+class Tests(unittest.TestCase):
+
+    sol = Solution()
+
+    def test_example_1(self):
+        self.assertEqual(self.sol.singleNumber([2,2,1]), 1)
+
+    def test_example_2(self):
+        self.assertEqual(self.sol.singleNumber([4,1,2,1,2]), 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
