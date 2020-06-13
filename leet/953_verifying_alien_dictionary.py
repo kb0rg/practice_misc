05820c305c11a4446ed595c6fd8cdb1ad049d5ed
kb0rg <kb0rg@users.noreply.github.com>
2020-06-13 15:10:29 -0700

    Convert examples to unit tests

--- leet/953_verifying_alien_dictionary.py
+++ leet/953_verifying_alien_dictionary.py
@@ -10,31 +10,6 @@ Given a sequence of words written in the alien language, and the order of the
 alphabet, return true if and only if the given words are sorted
 lexicographicaly in this alien language.
 
-
-Example 1:
-
-Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
-Output: true
-Explanation: As 'h' comes before 'l' in this language, then the sequence is
-sorted.
-
-Example 2:
-
-Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
-Output: false
-Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1],
-hence the sequence is unsorted.
-
-Example 3:
-
-Input: words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
-Output: false
-Explanation: The first three characters "app" match, and the second string is
-shorter (in size.) According to lexicographical rules "apple" > "app", because
-'l' > '∅', where '∅' is defined as the blank character which is less than any
-other character (More info).
-
-
 Constraints:
 
 1 <= words.length <= 100
@@ -42,6 +17,9 @@ Constraints:
 order.length == 26
 All characters in words[i] and order are English lowercase letters.
 """
+from typing import List
+import unittest
+
 
 class Solution:
     def compare_words(self, word_a, word_b, order_indexes):
@@ -57,7 +35,6 @@ class Solution:
             return True
         return False
 
-
     def isAlienSorted(self, words: List[str], order: str) -> bool:
         if len(words) == 1:
             return True
@@ -72,3 +49,49 @@ class Solution:
                 if not self.compare_words(word, next_word, order_indexes):
                     return False
         return True
+
+
+class TestSelfDividingNumber(unittest.TestCase):
+
+    sol = Solution()
+
+    def test_example_1(self):
+        """
+        Explanation: As 'h' comes before 'l' in this language, then the
+        sequence is sorted.
+        """
+        self.assertEqual(
+            self.sol.isAlienSorted(
+                ["hello","leetcode"],
+                "hlabcdefgijkmnopqrstuvwxyz"),
+            True)
+
+
+    def test_example_2(self):
+        """
+        Explanation: As 'd' comes after 'l' in this language, then
+        words[0] > words[1], hence the sequence is unsorted.
+        """
+        self.assertEqual(
+            self.sol.isAlienSorted(
+                ["word","world","row"],
+                "worldabcefghijkmnpqstuvxyz"),
+            False)
+
+
+    def test_example_3(self):
+        """
+        Explanation: The first three characters "app" match, and the second
+        string is shorter (in size.) According to lexicographical rules
+        "apple" > "app", because 'l' > '∅', where '∅' is defined as the blank
+        character which is less than any other character (More info).
+        """
+        self.assertEqual(
+            self.sol.isAlienSorted(
+                ["apple","app"],
+                "abcdefghijklmnopqrstuvwxyz"),
+            False)
+
+if __name__ == '__main__':
+
+    unittest.main()
