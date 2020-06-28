7a5ad18d31359873842ac69db0af031aa88842d4
kb0rg <kb0rg@users.noreply.github.com>
2020-06-28 12:09:01 -0700

    Convert example to unittest

--- leet/937_reorder_log_files.py
+++ leet/937_reorder_log_files.py
@@ -21,12 +21,6 @@ original order.
 Return the final order of the logs.
 
 
-Example 1:
-
-Input: logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
-Output: ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
-
-
 Constraints:
 
 0 <= logs.length <= 100
@@ -34,6 +28,22 @@ Constraints:
 logs[i] is guaranteed to have an identifier, and a word after the identifier.
 """
 
+import unittest
+
+from typing import List
+
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
         pass
+
+class Tests(unittest.TestCase):
+    sol = Solution()
+
+    def test_example_1(self):
+        input = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
+        expected_output = ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
+
+        self.assertEqual(self.sol.reorderLogFiles(input), expected_output)
+
+if __name__ == "__main__":
+    unittest.main()
